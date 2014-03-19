646c192dad69bdb68d3561f3a71c403ae3d0e536
Paul Nakata <pmn@github.com>
2014-03-18 21:43:16 -0700

    Formatted code

--- sparkles.go
+++ sparkles.go
@@ -1,18 +1,18 @@
 package main
 
 import (
-  "encoding/json"
+	"encoding/json"
 	"fmt"
+	"github.com/gorilla/mux"
 	"net/http"
 	"time"
-  "github.com/gorilla/mux"
 )
 
 type Sparkle struct {
-  Sparkler string `json:"sparkler"`
-  Sparklee string `json:"sparklee"`
-  Reason  string `json:"reason,omitempty"`
-  Time  time.Time `json:"time,omitempty"`
+	Sparkler string    `json:"sparkler"`
+	Sparklee string    `json:"sparklee"`
+	Reason   string    `json:"reason,omitempty"`
+	Time     time.Time `json:"time,omitempty"`
 }
 
 var sparkles []Sparkle
@@ -23,12 +23,12 @@ func defaultHandler(w http.ResponseWriter, h *http.Request) {
 
 func addSparkles(w http.ResponseWriter, h *http.Request) {
 	fmt.Fprint(w, "Add a sparkle")
-  var s Sparkle
-  b := json.NewDecoder(h.Body)
-  b.Decode(&s)
+	var s Sparkle
+	b := json.NewDecoder(h.Body)
+	b.Decode(&s)
 
-  sparkles = append(sparkles, s)
-  fmt.Printf("%v", sparkles)
+	sparkles = append(sparkles, s)
+	fmt.Printf("%v", sparkles)
 }
 
 func getSparkles(w http.ResponseWriter, h *http.Request) {
@@ -36,7 +36,7 @@ func getSparkles(w http.ResponseWriter, h *http.Request) {
 }
 
 func getSparklesForRecipient(w http.ResponseWriter, h *http.Request) {
-  vars := mux.Vars(h)
-  rcpt := vars["recipient"]
-  fmt.Fprint(w, "Get sparkles for ", rcpt)
+	vars := mux.Vars(h)
+	rcpt := vars["recipient"]
+	fmt.Fprint(w, "Get sparkles for ", rcpt)
 }
